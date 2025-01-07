fae6dfda6d6031fac056e56ca7579e1866e76f3f
Ashley Hayes <ashleymh060504@gmail.com>
2025-01-07 15:46:35 -0500

    edited .env file to add GitHyub API token, added useStates and useEffects and returned card to CandidateSearch, completed nav, added prelim css, created Candidate interface

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,8 +1,12 @@
 const Nav = () => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
-    <div>Nav</div>
+    <div className="links-container">
+      <a className="links" href="/">Home</a>
+      <a className="links"href="/SavedCandidates">Saved Candidates</a>
+    </div>
   )
 };
 
 export default Nav;
+
--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -1,8 +1,59 @@
 import { useState, useEffect } from 'react';
 import { searchGithub, searchGithubUser } from '../api/API';
+import Candidate from '../interfaces/Candidate.interface';
 
 const CandidateSearch = () => {
-  return <h1>CandidateSearch</h1>;
+
+  const [candidates, setCandidates] = useState<any[]>([]);
+
+  const [currentCandidate, setCurrentCandidate] = useState<Candidate>({
+    avatar_url: '',
+    login: '',
+    location: '',
+    email: '',
+    company: '',
+    bio: ''
+  });
+
+
+  useEffect(() => {
+    async function getUsers() {
+      const data = await searchGithub();
+      console.log(data);
+      setCandidates(data)
+    }
+    getUsers()
+  }, [])
+
+  useEffect(() => {
+    async function getUser() {
+      const data = await searchGithubUser(candidates[0].login);
+      console.log(data);
+      setCurrentCandidate(data)
+    }
+    getUser()
+  }, [candidates])
+
+
+  return (
+    <div className="main-container">
+      <h1>Candidate Search</h1>
+
+      <div className="card">
+        <img src={currentCandidate?.avatar_url} alt="placeholder" />
+        <h2>{currentCandidate?.login}</h2>
+        <p>Location: {currentCandidate?.location ? currentCandidate?.location : "(No location provided)"}</p>
+        <p>Email: {currentCandidate?.email ? currentCandidate?.email : "(No email provided)"}</p>
+        <p>Company: {currentCandidate?.company ? currentCandidate?.company : "(No company provided)"}</p>
+        <p>Bio: {currentCandidate?.bio ? currentCandidate?.bio : "(No bio provided)"}</p>
+      </div>
+
+      <div>
+        <button>Minus</button>
+        <button>Plus</button>
+      </div>
+    </div>
+  )
 };
 
 export default CandidateSearch;
